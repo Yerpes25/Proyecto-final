18ee7178c7155a555086399f39853f615e336efc
Yerpes25 <yeeeola@gmail.com>
2025-05-20 16:23:52 +0200

    Producto 1.0
    Creo producto con sus atributos

---
+++ proyecto/src/tiendaAOVE/Producto.java
@@ -0,0 +1,56 @@
+package tiendaAOVE;
+
+public class Producto {
+	private String nombre;
+	private Double precio;
+	private int stock;
+	private int valoracion;
+	
+	public Producto(String nombre, Double precio, int stock, int valoracion) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+		this.valoracion = valoracion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(Double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	public int getValoracion() {
+		return valoracion;
+	}
+
+	public void setValoracion(int valoracion) {
+		this.valoracion = valoracion;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [nombre=" + nombre + ", precio=" + precio + ", stock=" + stock + ", valoracion=" + valoracion
+				+ "]";
+	}
+	
+	
+}
