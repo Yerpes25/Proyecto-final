6a64f0f60777c8f9ee095ae74525649629a17c41
Yerpes25 <yeeeola@gmail.com>
2025-05-21 19:23:29 +0200

    App 1.2

--- proyecto/src/tiendaAOVE/App.java
+++ proyecto/src/tiendaAOVE/App.java
@@ -1,5 +1,10 @@
 package tiendaAOVE;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,7 +16,7 @@ public class App {
 	static ArrayList<Pedido> pedidos = new ArrayList<>();
 	static Cliente clienteAutenticado = null;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// TODO Auto-generated method stub
 		precargarProductos();
 		int opcion;
@@ -19,7 +24,7 @@ public class App {
 			menu();
 			System.out.print("Seleccione una opcion: ");
 			opcion = Integer.parseInt(entrada.nextLine());
-			
+
 			switch (opcion) {
 			case 1:
 				aniadirCliente();
@@ -33,11 +38,15 @@ public class App {
 			case 4:
 				comprarProducto();
 				break;
-			case 5: 
+			case 5:
 				mostrarPedidos();
 				break;
-			case 6: 
-				System.out.println("Has salido");
+			case 6:
+				sacarDatos();
+				break;
+			case 7:
+				meterDatos();
+				break;
 			default:
 				System.out.println("Opcion no valida.");
 			}
@@ -46,33 +55,37 @@ public class App {
 	}
 
 	private static void aniadirCliente() {
-		System.out.println("Dime el nombre");
-		String nombre = entrada.nextLine();
-		
-		System.out.println("Dime los apellidos");
-		String apellidos = entrada.nextLine();
-		
-		System.out.println("Dime el dni");
-		String dni = entrada.nextLine();
-		
-		System.out.println("Dime el telefono");
-		String telefono = entrada.nextLine();
-		
-		System.out.println("Dime el email");
-		String email= entrada.nextLine();
-		
-		if (buscarClientePorEmail(email) != null) {
-			System.out.println("Ya existe un cliente con ese email.");
-			return;
+		if (clienteAutenticado == null) {
+			System.out.println("Dime el nombre");
+			String nombre = entrada.nextLine();
+
+			System.out.println("Dime los apellidos");
+			String apellidos = entrada.nextLine();
+
+			System.out.println("Dime el dni");
+			String dni = entrada.nextLine();
+
+			System.out.println("Dime el telefono");
+			String telefono = entrada.nextLine();
+
+			System.out.println("Dime el email");
+			String email = entrada.nextLine();
+
+			if (buscarClientePorEmail(email) != null) {
+				System.out.println("Ya existe un cliente con ese email.");
+				return;
+			}
+
+			System.out.println("Dime la contraseña");
+			String contrasenia = entrada.nextLine();
+
+			Cliente c = new Cliente(email, contrasenia, nombre, apellidos, dni, telefono);
+			clientes.add(c);
+			System.out.println("Se ha añadido correctamente");
+		} else {
+			System.out.println("Ya has inciado sesionº");
 		}
-		
-		System.out.println("Dime la contraseña");
-		String contrasenia = entrada.nextLine();
-		
-		Cliente c = new Cliente(email, contrasenia, nombre, apellidos, dni, telefono);
-		clientes.add(c);
-		System.out.println("Se ha añadido correctamente");
-		
+
 	}
 
 	private static void menu() {
@@ -82,34 +95,40 @@ public class App {
 		System.out.println("3.- Mostrar productos");
 		System.out.println("4.- Comprar producto");
 		System.out.println("5.- Mostrar pedidos");
+		System.out.println("6.- Guardar Datos en archivo");
+		System.out.println("7.- Meter datos en archivo .ser");
 		System.out.println("6.- Salir");
 		System.out.println("************************");
 	}
-	
+
 	public static void inicioSesion() {
-		System.out.print("Email: ");
-		String email = entrada.nextLine();
+		if (clienteAutenticado == null) {
+			System.out.print("Email: ");
+			String email = entrada.nextLine();
 
-		System.out.print("Contraseña: ");
-		String contrasenia = entrada.nextLine();
+			System.out.print("Contraseña: ");
+			String contrasenia = entrada.nextLine();
 
-		Cliente cliente = buscarClientePorEmail(email);
+			Cliente cliente = buscarClientePorEmail(email);
 
-		if (cliente == null) {
-			System.out.println("El email no esta registrado");
-			return;
-		}
-		
-		int codigoContra = contrasenia.hashCode();
-		
-		if (cliente.getContrasenia() == (codigoContra)) {
-			System.out.println("Inicio de sesion correcto " + cliente.getNombre());
-			clienteAutenticado = cliente;
+			if (cliente == null) {
+				System.out.println("El email no esta registrado");
+				return;
+			}
+
+			int codigoContra = contrasenia.hashCode();
+
+			if (cliente.getContrasenia() == (codigoContra)) {
+				System.out.println("Inicio de sesion correcto " + cliente.getNombre());
+				clienteAutenticado = cliente;
+			} else {
+				System.out.println("Contraseña incorrecta");
+			}
 		} else {
-			System.out.println("Contraseña incorrecta");
+			System.out.println("Ya has inciado sesionº");
 		}
 	}
-	
+
 	private static Cliente buscarClientePorEmail(String email) {
 		for (Cliente c : clientes) {
 			if (c.getEmail().equalsIgnoreCase(email)) {
@@ -118,14 +137,14 @@ public class App {
 		}
 		return null;
 	}
-	
+
 	private static void precargarProductos() {
 		productos.add(new AceiteDeOliva("Aceite Virgen Extra", 10.0, 5, 0, TIPOACEITUNA.PICUAL, "Jaen"));
 		productos.add(new ProductosCosmetica("Crema", 15.0, 10, 0, "Con aloe vera"));
 		productos.add(new Accesorio("Botella de cristal", 8.0, 20, 0, "Vidrio"));
 		productos.add(new AceiteAromatizado("Aceite", 12.0, 7, 0, "Romero", 3));
 	}
-	
+
 	private static void mostrarProductos() {
 		System.out.println("********** Productos disponibles **********");
 		int i = 1;
@@ -176,5 +195,27 @@ public class App {
 				System.out.println(p);
 			}
 		}
+
 	}
+
+	private static void sacarDatos() throws IOException {
+		FileOutputStream fileOutputStream = new FileOutputStream("SacarDatos.ser");
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+		objectOutputStream.writeObject(clientes);
+		objectOutputStream.flush();
+		objectOutputStream.close();
+	}
+
+	private static void meterDatos() throws IOException, ClassNotFoundException {
+		FileInputStream fileInputStream = new FileInputStream("MeterDatos.ser");
+		ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+		@SuppressWarnings("unchecked")
+		ArrayList<Cliente> clientesLeidos = (ArrayList<Cliente>) objectInputStream.readObject();
+		objectInputStream.close();
+		
+		for(int i = 0; i < clientesLeidos.size(); i++) {
+			System.out.println(clientesLeidos.get(i));			
+		}
+	}
+
 }
