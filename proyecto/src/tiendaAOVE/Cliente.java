94a8b56d2221527f70024fbd459a6b3c938e045f
Yerpes25 <yeeeola@gmail.com>
2025-05-21 20:27:59 +0200

    Cliente 1.3

--- proyecto/src/tiendaAOVE/Cliente.java
+++ proyecto/src/tiendaAOVE/Cliente.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Cliente implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String email;
 	private int contrasenia;
 	private String nombre;
